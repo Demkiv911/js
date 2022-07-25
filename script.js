77abd8f3dafdb234f2711dee6c2d986a050930eb
Demkiv911 <demkiv.igo@gmail.com>
2022-07-25 11:00:45 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+let user = 'Ihor Demkiv';
+
+let h1User = document.querySelector(".user");
+h1User.innerHTML = user;
+
+
+
+
+// console.log(h1User.innerHTML);
+
+
+
+
+
+
+
+
+// let name    = 'Ihor';
+// let surname = 'Demkiv';
+// let age = 32;
+
+// let view = ""; //вивід тексту
+// let hello = "Привіт";
+// let _ = " ";
+// let label = "тобі вже";
+// let label2 = "років";
+
+// function sayMyName(myName= 'імені', mysurname = 'пріз') {
+//     let view ='привіт '+ myName + ' ' + surname;
+//     return data;
+// }
+
+// let view = sayMyName();
+// console.log('view', view);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(name){
+
+//     view = hello;
+//     view += _ + name;
+//     if (surname)
+//         view +=_+surname;
+    
+// } else{
+//     view = "імені немає";
+// }
+// if(age){
+//     view += _+ label +_+age +_+ label2;
+    
+// }
+
+// // view = hello;
+// // view += _ + name + _ + surname;
+// // view += _+ label +_+age +_+ label2;
+
+// console.log(view);
+
+
+// let test= true;
+
+// console.log(
+// (test)
+// ?"активна"
+// :"не активна"
+// );
